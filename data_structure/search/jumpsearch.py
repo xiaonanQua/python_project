0d0e5c10fd151e83b04ca529927c8fcfa440705a
github <1425877026@qq.com>
2019-07-15 20:56:38 +0800

    添加一些激活函数&补充了跳转算法

--- data_structure/search/jumpsearch.py
+++ data_structure/search/jumpsearch.py
@@ -1,8 +1,17 @@
 """
 跳转搜索(Jump Search),跳转搜索是搜索的一种算法，其查找的数组都是已经排好序的数组
 基本思想：按固定的步长跳过一些数组中的元素从而检索更少的元素。
-例如，假设有n长度的数据arr，跳转步长为m，然后我们搜索索引arr[0],arr[m],arr[2m],..arr[km]等等。
-一旦我们发现间隔为（arr[km]<x<arr[(k+1)m]）,我们从索引km开始执行线性搜索去查找元素x。
+    例如，假设有n长度的数据arr，跳转步长为m，然后我们搜索索引arr[0],arr[m],arr[2m],..arr[km]等等。
+    一旦我们发现间隔为（arr[km]<x<arr[(k+1)m]）,我们从索引km开始执行线性搜索去查找元素x。
+问题：进行跳过的步长，设置成多少是最好？
+    在最坏的情况下，我们不得不进行n/m(n:数组长度，m跳过步长)次跳过。如果搜索到的值正好是m索引的前一个，
+    则需要进行m-1比较。因此，在最差情况下，总体的比较次数为（n/m）+(m-1)。当m=sqrt(n)时，那个函数的值
+    将是最小。因此，最好的步长是m=sqrt(n)
+重要点：
+    （1）这个算法仅仅支持排好序的数组
+    （2）跳转的最优步长为sqrt(n)[对n开根号]。这个使jump search的时间复杂度为O(sqrt(n))
+    （3）跳转算法的时间复杂度是介于线性搜索（顺序，O(n)）和二分搜索（O(log(n))）之间
+    （4）二分搜索比跳转搜索更好，但是在一个跳回很繁琐的情况下，使用跳转搜索更好。
 """
 
 import math
@@ -20,7 +29,7 @@ def jump_search(data, key):
     step = math.sqrt(length)
     pre_step_index = 0  # 保存前一个步长所在的索引
     # min()函数的使用，是选取最小值，防止步长越界，使索引保持在length内
-    while data[int(min(step, length))] < key:  # 判断数据存在查找关键词的条件
+    while data[int(min(step, length))-1] < key:  # 判断数据存在查找关键词的条件
         pre_step_index = step  # 保存当前步长索引
         step = step + math.sqrt(length)  # 更新下一个步长索引
         if pre_step_index >= length:  # 若前一个步长索引超过数组长度，则表明查找的值不存在
@@ -41,4 +50,4 @@ def jump_search(data, key):
 
 if __name__ == '__main__':
     data = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    print(jump_search(data, 7))
+    print(jump_search(data, 9))
--- tensorflow_learn/basic_tensorflow/math_func.py
+++ tensorflow_learn/basic_tensorflow/math_func.py
@@ -104,20 +104,90 @@ def relu_func(data, param):
     relu = []
     leaky_relu = []
     par_relu = []
+    i = 0
     # 计算函数
-    for i, x in data:
+    for x in data:
         relu.append(max(x, 0))
-        leaky_relu.append(x, 0.01*x)
-        par_relu.append(max(x, param[i]*x))
+        leaky_relu.append(max(x, 0) + 0.01*min(x, 0))
+        par_relu.append(max(x, 0) + param[i]*min(x, 0))
+        i = i + 1
+    # 可视化函数图像
+    plt.title('relu、leaky_relu and par_relu graph')
+    plt.plot(data, relu, label='ReLU')
+    plt.plot(data, leaky_relu, label='LeakyReLU')
+    plt.plot(data, par_relu, label='ParReLU')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.show()
+
+
+def elu_func(data):
+    """
+    指数线性单元
+    :param data:数据
+    :return:
+    """
+    # 参数初始化
+    elu = []
+    # 计算函数值
+    for x in data:
+        elu.append(max(x,0)+min(0, 0.02*(math.exp(x)-1)))
+    # 实现函数图像
+    plt.title('ELU graph')
+    plt.plot(data, elu, label='ELU')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.show()
+
+
+def softplus_func(data):
+    """
+    softplus是修正函数的平滑版本，其导数刚好是logistic
+    :param data:数据
+    :return:
+    """
+    # 初始化参数
+    softplus = []
+    # 进行函数计算
+    for x in data:
+        softplus.append(math.log(1+math.exp(x)))
+    # 可视化函数图像
+    plt.title('Softplus graph')
+    plt.plot(data, softplus, label='Softplus')
+    plt.legend()
+    plt.show()
+
+
+def swish_func(data):
+    """
+    swish函数是一种自门控的激活函数，swish(x)=xγ(βx),其中γ是Logistic函数，β为可学习或者固定参数
+    :param data:
+    :return:
+    """
+    gama = [0, 0.5, 1, 50]  # 定义超参数
+    for g in gama:
+        swish = []  # 存储函数值
+        for x in data:
+            logistic = 1/(1+math.exp(-g*x))
+            swish.append(x*logistic)
+        plt.plot(data, swish, label='β:{}'.format(g))
+    plt.title('Swish graph')
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
     data = np.linspace(-10, 10, 100)
-    param = np.linspace(0, 1, 100, endpoint=False)
     param = np.random.rand(100)
     # print(data)
-    print(param)
+    # print(param)
     # standard_logistic_func()
     # logistic_func(data, L=1, x_0=-1, k=1)
     # logistic_and_tanh_func(data)
-    hard_logistic_and_tanh(data)
+    # hard_logistic_and_tanh(data)
+    # relu_func(data, param)
+    # elu_func(data)
+    # softplus_func(data)
+    swish_func(data)
