27b15aba3f046ebece695fddbd4d7acb520ac0d5
xiaonanQua <1425877026@qq.com>
2019-07-16 23:00:06 +0800

    前馈网络的学习标记

--- tensorflow_learn/machine_learning/linearregression.py
+++ tensorflow_learn/machine_learning/linearregression.py
@@ -37,14 +37,44 @@ def run_steep_gradient_descent(data_x, data_y, len_data, alpha, theta):
     :return:更新特征，使用当前特征-alpha×特征梯度
     """
     n = len_data
-    pass
+    print(data_x.transpose())
+    prod = np.dot(theta, data_x.transpose())  # theta和x的乘积
+    print(prod.shape, data_y.shape, data_y.transpose().astype(float).shape)
+    prod -= data_y.transpose().astype(float)
+
+    print(prod)
+
+
+def run_linear_regression(data_x, data_y):
+    """
+    对数据集实现线性回归
+    :param data_x:包含数据集
+    :param data_y:包含输出（结果向量）
+    :return:最佳拟合直线特征（特征向量）
+    """
+    iterations = 100000  # 训练次数
+    alpha = 0.000155  # 学习率
+
+    no_features = data_x.shape[1]  # 特征类别
+    len_data = data_x.shape[0] - 1  # 数据长度
+    theta = np.zeros((1, no_features))  # 参数初始化
+    # 利用梯度下降学习参数theta和平方误差和
+    for i in range(0, iterations):
+        theta = run_steep_gradient_descent(data_x, data_y, len_data, alpha, theta)
+        break
+    print(theta)
+    print(no_features)
+    print(len_data)
 
 
 def main():
     """主函数"""
     data = collect_dataset()
     len_data = data.shape[0]  # 矩阵的形状是个元组形式，第一项代表数据长度
+    data_x = np.c_[np.ones(len_data), data[:, :-1]].astype(float)  # 把数据的第一列的前len-1个数切割出来，并转化成float型
+    data_y = data[:, -1]  # 切割保留每一行的最后一个数
+    theta = run_linear_regression(data_x, data_y)
 
 
 if __name__ == '__main__':
-    collect_dataset()
+    main()
